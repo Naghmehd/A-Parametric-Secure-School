e44ced733247c87f48d19fa1d7eba47cbf55d8f3
Naghmeh Shirazi <naghmehd@gmail.com>
2016-04-06 08:40:38 -0500

    redid router

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,10 @@
 Rails.application.routes.draw do
 
   resources :schools do
-      resources :teachers do
-        resources :students do
-          resources :courses do
-        end
-        end
-      end
-    end
+    resources :teachers
+    resources :students
+    resources :courses 
+  end
 
   root 'schools#index'
 
